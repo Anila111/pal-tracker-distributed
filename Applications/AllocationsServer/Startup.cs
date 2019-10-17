de6d9bf955a9a79b92bae182ec454907b0972290
PAL-5 <surisetty_chaitanya@dell.com>
2019-10-17 16:33:12 +0530

    CircuitBreaker1.1

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -45,6 +45,7 @@ namespace AllocationsServer
             });
             services.AddDiscoveryClient(Configuration);
              services.AddHystrixMetricsStream(Configuration);
+             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
